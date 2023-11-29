ceafaf950b85f8dfd0a958990b9b58aee8abf043
Pirota Pirozou <tm_rev@hotmail.com>
2023-11-29 19:25:58 +0900

    first commit

---
+++ CConvert.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using NPOI.OpenXmlFormats.Spreadsheet;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HPSF;
+using NPOI.OpenXmlFormats.Wordprocessing;
+using System.Runtime.Serialization;
+using Debug = System.Diagnostics.Debug;
+using System.Diagnostics.Metrics;
+using static NPOI.HSSF.Util.HSSFColor;
+
+
+namespace xlsx2csv
+{
+	internal class CConvert
+	{
+		/// <summary>
+		/// セルのint値を取得する
+		/// </summary>
+		int GetCellInt(ICell cell)
+		{
+			int ret = 0;
+			if (cell != null)
+			{
+				int.TryParse(cell.ToString(), out ret);
+			}
+			return ret;
+		}
+
+		/// <summary>
+		/// セルのstring値を取得する
+		/// </summary>
+		string GetCellString(ICell cell)
+		{
+			string ret = "";
+			if (cell != null)
+			{
+				ret = cell.ToString();
+			}
+			return ret;
+		}
+
+		/// <summary>
+		/// 32bitのエンディアンを変換する
+		/// </summary>
+		int SwapEndian32(int value)
+		{
+			byte[] bytes = BitConverter.GetBytes(value);
+
+			if (BitConverter.IsLittleEndian)
+			{
+				System.Array.Reverse(bytes);
+			}
+
+			return BitConverter.ToInt32(bytes, 0);
+		}
+
+		/// <summary>
+		/// 16bitのエンディアンを変換する
+		/// </summary>
+		short SwapEndian16(short value)
+		{
+			byte[] bytes = BitConverter.GetBytes(value);
+
+			if (BitConverter.IsLittleEndian)
+			{
+				System.Array.Reverse(bytes);
+			}
+
+			return BitConverter.ToInt16(bytes, 0);
+		}
+
+
+		/// <summary>
+		/// 変換処理
+		/// </summary>
+		/// <param name="infile">入力ファイル</param>
+		/// <param name="outfile">出力ファイル</param>
+		internal void DoConvert(string infile, string outfile)
+		{
+			int columnCount = 0;
+			List<string> csvlist = new List<string>();
+
+			// 入力ファイル存在チェック
+			if (!File.Exists(infile))
+			{
+				Console.WriteLine($"'{infile}' not found.");
+				return;
+			}
+
+			// ファイルを開く
+			// 他のプロセスでファイルを開いているときでも、読み取り可能にする
+			using (FileStream file = new FileStream(infile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			{
+				IWorkbook workbook = new XSSFWorkbook(file); // ワークブックを読み込む
+				ISheet sheet = workbook.GetSheetAt(0); // 最初のシートを取得
+
+				// 行と列をループしてセルの値を取得
+				for (int i = sheet.FirstRowNum; i <= sheet.LastRowNum; i++)
+				{
+					IRow row = sheet.GetRow(i);
+					if (i == sheet.FirstRowNum)
+					{
+						// ヘッダー行を読み飛ばす
+						// カラム行数の取得
+						columnCount = row.LastCellNum - row.FirstCellNum;
+						Debug.WriteLine($"colomnCount={columnCount}");
+						continue;
+					}
+
+					if (row != null)
+					{
+						string csv = String.Empty;
+						for (int j=0; j<columnCount; j++)
+						{
+							string tmp = GetCellString(row.GetCell(j));
+							csv += tmp;
+							if (j < (columnCount - 1))
+							{
+								csv += ",";
+							}
+						}
+						if (csv == String.Empty) { break; } // IDが未指定であれば、終端マークとみなし終了
+						Debug.WriteLine(csv);
+						csvlist.Add(csv);
+					}
+				} // 行と列をループしてセルの値を取得の終わり
+			} // usingの終わり
+
+			// UTF-8 エンコーディングでファイルに書き込み
+			using (StreamWriter writer = new StreamWriter(outfile, false, Encoding.UTF8))
+			{
+				foreach (var line in csvlist)
+				{
+					writer.WriteLine(line);
+				}
+			}
+			Console.WriteLine($"'{outfile}' wrote.");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿//---------------------------------------------------------------------------
+/** @file Program.cs
+ * xlsx から、UTF-8のcsvに変換するコンバータ
+ *
+ * $Id: Program.cs 2023-11-29 18:25:00Z maru $
+ *
+ *	@author $Author: maru $
+ *  @date $Date:: 2023-11-19 18:10:27 +0900#$
+ *  @version $Revision: n $
+ *
+ **/
+//---------------------------------------------------------------------------
+using System;
+using System.Text;
+using System.IO;
+using System.Collections;
+using Debug = System.Diagnostics.Debug;
+using System.Globalization;
+
+namespace xlsx2csv
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("xlsx2csv by Pirota");
+			string ext = null;
+			string infile = null;   // 入力ファイル名
+			string outfile = null;   // 出力ファイル名
+
+			// オプション解析
+			for (int i = 0; i < args.Length; i++)
+			{
+				char ch = args[i][0];
+				// スイッチ判定
+				if ((ch == '-' || ch == '/') && args[i].Length >= 2)
+				{
+					char opt = args[i][1];
+
+					switch (opt)
+					{
+						case '?':
+							usage();
+							break;
+
+						default:
+							Console.WriteLine("Missing option '{0}'.", opt);
+							Environment.Exit(1);
+							break;
+					}
+
+				}
+				else
+				{
+					// 入力ファイル名
+					if (infile == null)
+					{
+						infile = new string(args[i]);
+					}
+					else
+					{
+						// 出力ファイル
+						if (outfile == null)
+						{
+							outfile = new string(args[i]);
+						}
+					}
+
+				}
+			}
+			// 使用法
+			if (args.Length == 0 || infile == null)
+			{
+				usage();
+
+			}
+
+			// 出力ファイル名が未指定だったら、入力からコピー
+			if (outfile == null)
+			{
+				outfile = Path.GetFileNameWithoutExtension(infile);
+			}
+
+			// 拡張子チェック
+			ext = Path.GetExtension(infile);
+			if (ext == "")
+			{
+				infile += ".xlsx";
+			}
+			ext = Path.GetExtension(outfile);
+			if (ext == "")
+			{
+				outfile += ".csv";
+			}
+
+			//
+#if DEBUG
+			Debug.WriteLine("infile={0} outfile={1}", infile, outfile);
+#endif
+
+#if NETCOREAPP
+			// コードページ エンコーディング プロバイダーを登録
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#endif
+
+			// 変換開始
+			CConvert cmp = new CConvert();
+
+			cmp.DoConvert(infile, outfile);
+
+			cmp = null;
+			GC.Collect();
+
+		}
+
+		/// <summary>
+		/// 使用法の表示
+		/// </summary>
+		private static void usage()
+		{
+			Console.WriteLine("usage: xlsx2csv [infile].xlsx [outfile].csv");
+
+			Environment.Exit(0);
+		}
+
+	}
+}
